7cf56c0684942b95b8e791c9ff59ad57b524987e
ThaisNogueiraB <thais.nogueirab@hotmail.com>
2023-03-10 19:05:41 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,15 @@
+var swiper = new Swiper(".slide-principais", {
+    slidesPerView: 3,
+    spaceBetween: 150,
+   
+  });
+
+  var swiper = new Swiper(".slide-principais-grande", {
+    slidesPerView: 4,
+  });
+
+  var swiper = new Swiper(".slide-principais-grande-grande", {
+    slidesPerView: 5,
+  });
+
+ 
